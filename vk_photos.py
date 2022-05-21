29fd43d6bef3fb8098ca1fc62217a029cd164f13
ismoilovmf <muxri1334@mail.ru>
2022-05-21 18:30:42 +0300

    Add function

--- vk_photos.py
+++ vk_photos.py
@@ -1,14 +1,15 @@
-import requests
-from pprint import pprint
 from fake_useragent import UserAgent
+from pprint import pprint
+from tqdm import tqdm
+import requests
 import time
 
 
 with open("/home/ismoilov_m_f/Python/tokens/vktoken.txt") as f:
-    vk_token = f.read().split()[0]
+    vk_token = f.readline().strip()
 
 with open("/home/ismoilov_m_f/Python/tokens/yadisk.txt") as f:
-    yadisk_token = f.read().split()[0]
+    yadisk_token = f.readline().strip()
 
 
 class Vk_class():
@@ -23,35 +24,35 @@ class Vk_class():
             "access_token": vk_token,
             "v": "5.131"
         }
-        self.name_folder = time.strftime('%Y-%m-%d_%H-%M-%S')
+        self.name_f = "VK " + time.strftime('%Y-%m-%d_%H-%M-%S')
         self._host_yan = "https://cloud-api.yandex.net"
         self.__header_yan = {"User-Agent": UserAgent().random,
                              "Content-Type": "application/json",
                              "Authorization": f"OAuth {yadisk_token}"
                              }
+        self.name_folder = self.create_folder(self.name_f)
 
     def create_folder(self, name_folder: str):
         url = "https://cloud-api.yandex.net/v1/disk/resources"
         self.params = {"path": name_folder}
         resp = requests.put(url, params=self.params, headers=self.__header_yan)
-        return resp.status_code  # 201 >>> normal
+        if resp.status_code == 201:  # or resp.status_code == 409:
+            return name_folder
+        else:
+            time.sleep(1)
+            self.create_folder(self.name_f+time.strftime('%Y-%m-%d_%H-%M-%S'))
 
-    def yan_uploader(self, img_name, img_url):
+    def _yan_uploader(self, name_portal, img_name, img_url):
 
         url = f"{self._host_yan}/v1/disk/resources/upload/"
-        folder_name = time.strftime('%Y-%m-%d_%H-%M-%S')
-
-        code = self.create_folder(folder_name)
-        while code != 201:
-            folder_name += "(1)"
-            code = self.create_folder(folder_name)
+        folder_name = name_portal + time.strftime(self.name_folder)
 
         name = img_name
         params = {
-            "path": f"{folder_name}/{name}", "url": img_url, "overwrite": True}
+            "path": f"{self.name_folder}/{name}", "url": img_url, "overwrite": True}
         response = requests.post(url, params=params, headers=self.__header_yan)
-        if response.status_code == 202:
-            print("Completed")
+        # if response.status_code == 202:
+        #     print("Completed")
 
     def get_photos(self):
         url = self._host_vk+"/photos.get"
@@ -70,7 +71,7 @@ class Vk_class():
             return response
 
         def resp_handler(resp):
-            for resp in resp["items"]:
+            for resp in tqdm(resp["items"]):
                 name_photo = f'{resp["likes"]["count"]}.jpg'
                 url_photo = str(resp["sizes"][-1]["url"])
                 if not name_photo in self.dct:
@@ -79,13 +80,13 @@ class Vk_class():
                     date = time.strftime('%Y-%m-%d_%H-%M-%S',
                                          time.localtime(resp["date"]))
                     self.dct[f'{resp["likes"]["count"]}_{date}.jpg'] = url_photo
-
+                    # time.sleep(0.1)
+                self._yan_uploader("VK", name_photo, url_photo)
         resp_lst = get_resp()
         count_photo = resp_lst["count"]
         offset = 999
         while count_photo > 998:
-            for resp in resp_lst["items"]:
-                resp_handler(resp_lst)
+            resp_handler(resp_lst)
             resp_lst = get_resp(offset=offset, count=999)
             offset += 999
             count_photo -= offset
@@ -96,7 +97,7 @@ class Vk_class():
                 print("No photos or no access to photos")
         pprint(self.dct)
         print(len(self.dct))
-        print(count_photo)
+        # print(count_photo)
 
 
 #
